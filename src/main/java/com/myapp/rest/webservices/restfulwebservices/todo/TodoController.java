5831ab09c2202cf1ba50e789cf38ca2736f3866a
Breno Gianotto <breno.gntt@gmail.com>
2019-09-03 00:48:51 -0300

    Third commit

--- src/main/java/com/myapp/rest/webservices/restfulwebservices/todo/TodoController.java
+++ src/main/java/com/myapp/rest/webservices/restfulwebservices/todo/TodoController.java
@@ -1,6 +1,7 @@
 package com.myapp.rest.webservices.restfulwebservices.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +19,11 @@ public class TodoController {
         return todoHardCodedService.findAll();
     }
 
+    @GetMapping(path="/users/{username}/todos/{id}")
+    public Todo getAllTodos(@PathVariable  String username, @PathVariable Long id){
+        return todoHardCodedService.findById(id);
+    }
+
     @DeleteMapping(path="/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
         Todo todo =  todoHardCodedService.deleteById(id);
@@ -27,5 +33,14 @@ public class TodoController {
         } else{
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PutMapping(path="/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(
+            @PathVariable String username,
+            @PathVariable long id,
+            @RequestBody Todo todo){
+        Todo todoUpdated = todoHardCodedService.save(todo);
+        return new ResponseEntity<Todo>(todoUpdated,HttpStatus.OK);
     }
 }
